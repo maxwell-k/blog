4b7e024cbdeb94f3232e0a329e5e8fb0bcdd9533
Keith Maxwell <keith.maxwell@gmail.com>
2024-09-19 12:47:31 +0100

    Refactor gulpfile.js

--- theme/gulpfile.js
+++ theme/gulpfile.js
@@ -1,25 +1,28 @@
-var gulp = require('gulp');
+const gulp = require("gulp");
 
-var paths = {
-    styles: ['src/**/*.css']
-}
+const paths = {
+  styles: ["src/**/*.css"],
+};
 
-gulp.task('default', ['css']);
+gulp.task("default", ["css"]);
 
-gulp.task('watch', function() {
-    gulp.watch(paths.styles, ['css']);
+gulp.task("watch", function () {
+  gulp.watch(paths.styles, ["css"]);
 });
 
-gulp.task('css', function () {
-    var postcss = require('gulp-postcss');
-    var sourcemaps = require('gulp-sourcemaps');
+gulp.task("css", function () {
+  const postcss = require("gulp-postcss");
+  const sourcemaps = require("gulp-sourcemaps");
 
-    return gulp.src(paths.styles)
-        .pipe(sourcemaps.init())
-        .pipe(postcss([
-            require('stylelint'),
-            require('postcss-import'),
-            require('cssnano')
-        ]))
-        .pipe(gulp.dest('.'))
+  return gulp
+    .src(paths.styles)
+    .pipe(sourcemaps.init())
+    .pipe(
+      postcss([
+        require("stylelint"),
+        require("postcss-import"),
+        require("cssnano"),
+      ]),
+    )
+    .pipe(gulp.dest("."));
 });
