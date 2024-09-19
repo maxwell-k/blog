683315ab5ec90db39368b2e1367929b3d40fa8bf
Keith Maxwell <keith.maxwell@gmail.com>
2024-09-19 16:35:53 +0100

    `npm exec gulp watch` launches pelican

--- gulpfile.cjs
+++ gulpfile.cjs
@@ -1,3 +1,4 @@
+const { spawn } = require("child_process");
 const gulp = require("gulp");
 const cssnano = require("cssnano");
 const stylelint = require("stylelint");
@@ -17,8 +18,29 @@ function css() {
     .pipe(gulp.dest("theme"));
 }
 
-gulp.task("watch", function () {
+function csswatch() {
   gulp.watch(paths.styles, css);
-});
+}
+
+function pelican(cb) {
+  const cmd = spawn(
+    "uvx",
+    [
+      "--with-requirements=requirements.txt",
+      "pelican",
+      "--autoreload",
+      "--listen",
+    ],
+    { stdio: "inherit" },
+  );
+  cmd.on("close", function (code) {
+    console.log("Server exited with code " + code);
+    cb(code);
+  });
+}
+
+gulp.task("pelican", pelican);
+
+gulp.task("watch", gulp.parallel("pelican", csswatch));
 
 gulp.task("default", css);
