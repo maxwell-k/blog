ac4c403a8fcd5805318797e903613cb28a98cee2
Keith Maxwell <keith.maxwell@gmail.com>
2024-09-29 13:49:32 +0100

    More closely follow gulp documentation
    Including for example:
    <https://gulpjs.com/docs/en/getting-started/creating-tasks>

--- gulpfile.cjs
+++ gulpfile.cjs
@@ -1,6 +1,6 @@
 const { rimraf } = require("rimraf");
 const { spawn } = require("child_process");
-const gulp = require("gulp");
+const { dest, parallel, series, src, watch } = require("gulp");
 const cssnano = require("cssnano");
 const stylelint = require("stylelint");
 const postcssBundler = require("@csstools/postcss-bundler");
@@ -11,7 +11,7 @@ const paths = {
   unused: ["theme/static/css/_*.css"],
 };
 
-gulp.task("pelican", (cb) => {
+function pelican(cb) {
   const cmd = spawn(
     "uv",
     [
@@ -28,23 +28,17 @@ gulp.task("pelican", (cb) => {
     console.log("Server exited with code " + code);
     cb(code);
   });
-});
+}
 
-const css = gulp.series(
-  () =>
-    gulp
-      .src(paths.styles)
-      // .pipe(sourcemaps.init())
-      .pipe(postcss([stylelint, postcssBundler, cssnano]))
-      // .pipe(sourcemaps.write()) // to debug in Chrome Developer Tools
-      .pipe(gulp.dest("theme")),
-  () => rimraf(paths.unused, { glob: true }),
-);
+const pipeline = () =>
+  src(paths.styles)
+    .pipe(postcss([stylelint, postcssBundler, cssnano]))
+    .pipe(dest("theme"));
+const clean = () => rimraf(paths.unused, { glob: true });
+const css = series(pipeline, clean);
+const watch_css = () => watch(paths.styles, css);
 
-gulp.task(
-  "watch",
-  gulp.parallel("pelican", () => gulp.watch(paths.styles, css)),
-);
-
-gulp.task("css", css);
-gulp.task("default", css);
+exports.css = css;
+exports.pelican = pelican;
+exports.watch = parallel(pelican, watch_css);
+exports.default = css;
