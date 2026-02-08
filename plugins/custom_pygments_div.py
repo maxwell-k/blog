ef467ef6f5ba452e305def819658f39624c62d4a
Keith Maxwell <keith.maxwell@gmail.com>
2026-02-08 11:20:14 +0000

    Remove extra blank lines

--- plugins/custom_pygments_div.py
+++ plugins/custom_pygments_div.py
@@ -1,9 +1,12 @@
-"""Remove empty spans from HTML output.
+"""Customise pygments code blocks.
 
-So that tidy doesn't report an error when viewing output files.
+1. Remove empty spans from HTML output. So that tidy doesn't report an error
+   when viewing output files. These spans are added by [1].
+2. Insert a copy button. So that src/copy.js can listen for clicks and copy the
+   relevant contents to the clipboard.
+3. Remove extra blank lines after the div, so that djlint doesn't complain.
 
-These spans are added by:
-https://github.com/pygments/pygments/blob/2.18.0/pygments/formatters/html.py#L813
+[1]: https://github.com/pygments/pygments/blob/2.18.0/pygments/formatters/html.py#L813
 """
 
 import logging
@@ -13,14 +16,15 @@ from pelican import signals
 
 logger = logging.getLogger(__name__)
 
+BUTTON = "</code></pre><button>Copy 📋</button></div>"
 PAIRS = (
-    (
+    (  # empty spans
         '<div class="highlight"><pre><span></span><code>',
         '<div class="highlight"><pre><code>',
     ),
-    (
+    (  # insert the copy button
         "</code></pre></div>",
-        "</code></pre><button>Copy 📋</button></div>",
+        BUTTON,
     ),
 )
 
@@ -33,13 +37,23 @@ def custom_pygments_div(path_: str, context: dict) -> None:
 
     if context["DEBUG"]:
         logger.debug("Processing %s", path)
+
     lines = path.read_text().splitlines()
+
     for before, after in PAIRS:
         for i in range(len(lines)):
             line = lines[i]
             if line.startswith(before):
                 lines[i] = after + line.removeprefix(before)
-    path.write_text("\n".join(lines))
+
+    # only one blank line after the button
+    output = []
+    for line in lines:
+        if not line and output[-2:] == [BUTTON, ""]:
+            continue
+        output.append(line)
+
+    path.write_text("\n".join(output))
 
 
 def register() -> None:
